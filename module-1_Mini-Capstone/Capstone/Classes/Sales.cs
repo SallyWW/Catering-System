34588f9ab080ccd872f6600ef7e4e1c220e03c49
Brian Duffy <duffy.130@gmail.com>
2021-06-04 17:14:31 -0400

    started work on the bonus part of the capstone

---
+++ module-1_Mini-Capstone/Capstone/Classes/Sales.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Classes
+{
+    public class Sales
+    {
+        public string Name { get; private set; }
+        public int QuantitySold { get; private set; }
+        public double ItemSales { get; private set; }
+
+        public Sales(string name, int quantitySold, double ItemSales)
+        {
+            this.Name = name;
+            this.QuantitySold = quantitySold;
+            this.ItemSales = ItemSales;
+        }
+
+
+    }
+}
---
+++ module-1_Mini-Capstone/Capstone/Classes/TotalSales.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Classes
+{
+    public class TotalSales
+    {
+        public double RunningTotal { get; private set; }
+
+        private List<Sales> salesLog = new List<Sales>();
+        private List<TransactionLog> customerTransaction = new List<TransactionLog>();
+
+
+        public void AddSale(Sales currentSale)
+        {
+            salesLog.Add(currentSale);
+        }
+
+        public void SetCustomerTransaction(Customer currentCustomer)
+        {
+            customerTransaction = currentCustomer.GetTransactions();
+        }
+
+
+
+    }
+}
